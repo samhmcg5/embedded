d752952d84f7c494935b255d18ab6585a716758f
Samuel McGhee <samhmcg5@vt.edu>
2017-09-10 21:32:01 -0400

    deleting build files, working on merge still

--- milestone1/firmware/basic_uart.X/build/default/production/_ext/410364714/port_asm.o.d
+++
@@ -1,9 +0,0 @@
-build/default/production/_ext/410364714/port_asm.o:  \
- ../../../../../../../../microchip/harmony/v2_04/third_party/rtos/FreeRTOS/Source/portable/MPLAB/PIC32MX/port_asm.S  \
- ../../../../../../../../microchip/harmony/v2_04/third_party/rtos/FreeRTOS/Source/portable/MPLAB/PIC32MX/ISR_Support.h  \
- ../src/system_config/default/FreeRTOSConfig.h 
-build/default/production/_ext/410364714/port_asm.o:  \
- ../src/system_config/default/FreeRTOSConfig.h  \
- ../../../../../../../../microchip/harmony/v2_04/third_party/rtos/FreeRTOS/Source/portable/MPLAB/PIC32MX/ISR_Support.h  \
-  \
- ../../../../../../../../microchip/harmony/v2_04/third_party/rtos/FreeRTOS/Source/portable/MPLAB/PIC32MX/port_asm.S  \
