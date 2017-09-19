72cc9828f9d8bf49cf45e71a019506ef7ab530d8
Samuel McGhee <samhmcg5@vt.edu>
2017-09-19 14:56:45 -0400

    motor control api using register writes

---
+++ pwm_registers/firmware/src/app.c
@@ -0,0 +1,107 @@
+#include "app.h"
+
+
+APP_DATA appData;
+
+void initMotor1and2()
+{
+    // default directions to forward
+    // Motor 1 direction 0x4000
+    TRISCCLR = MOTOR1_DIR_PIN;
+    ODCCCLR  = MOTOR1_DIR_PIN;
+    LATCCLR  = MOTOR1_DIR_PIN;
+
+    // motor 2 direction 0x0002
+    TRISGCLR = MOTOR2_DIR_PIN;
+    ODCGCLR  = MOTOR2_DIR_PIN;
+    LATGCLR  = MOTOR2_DIR_PIN;
+    
+    T2CONSET = 0x0070;   // set for 256 prescale
+    PR2 = TMR2_PERIOD;  // set the period to 3125
+    // default motors to OFF
+    OC1CON = 0x0000;    // turn off while setting up
+    OC1R = 0x0000;      // primary compare register
+    OC1RS = 0x0000;     // secondary compare register
+    OC1CON = 0x0006;    // configure for PWM mode
+    
+    OC2CON = 0x0000;    // turn off while setting up
+    OC2R = 0x0000;      // primary compare register
+    OC2RS = 0x0000;     // secondary compare register
+    OC2CON = 0x0006;    // configure for PWM mode
+    
+    T2CONSET = 0x8000;  // enable timer 2
+    OC1CONSET = 0x8000; // enable OC1
+    OC2CONSET = 0x8000; // enable OC2
+}
+
+void setMotor1Forward()
+{
+    LATCCLR  = MOTOR1_DIR_PIN;
+}
+
+void setMotor2Forward()
+{
+    LATGCLR  = MOTOR2_DIR_PIN;
+}
+
+void setMotor1Backward()
+{
+    LATCCLR  = MOTOR1_DIR_PIN;
+    LATCSET  = MOTOR1_DIR_PIN;
+}
+
+void setMotor2Backward()
+{
+    LATGCLR  = MOTOR2_DIR_PIN;
+    LATGSET  = MOTOR2_DIR_PIN;
+}
+
+void setMotor1DC(unsigned char dc)
+{
+    float f_dc = (float)dc / 100.0;
+    short int new_ocrs = TMR2_PERIOD * f_dc;
+    OC1RS = new_ocrs;
+}
+
+void setMotor2DC(unsigned char dc)
+{
+    float f_dc = (float)dc / 100.0;
+    short int new_ocrs = TMR2_PERIOD * f_dc;
+    OC2RS = new_ocrs;
+}
+
+void APP_Initialize ( void )
+{
+    /* Place the App state machine in its initial state. */
+    appData.state = APP_STATE_INIT;
+    initMotor1and2();
+}
+
+void APP_Tasks ( void )
+{
+    switch ( appData.state )
+    {
+        case APP_STATE_INIT:
+        {
+            bool appInitialized = true;
+            if (appInitialized)
+            {
+                appData.state = APP_STATE_SERVICE_TASKS;
+            }
+            break;
+        }
+
+        case APP_STATE_SERVICE_TASKS:
+        {
+            setMotor1DC(100);
+            setMotor2DC(50);
+            break;
+        }
+
+        default:
+        {
+            break;
+        }
+    }
+}
+
---
+++ pwm_registers/firmware/src/app.h
@@ -0,0 +1,54 @@
+#ifndef _APP_H
+#define _APP_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include "system_config.h"
+#include "system_definitions.h"
+
+#define MOTOR1_DIR_PIN     0x4000
+#define MOTOR2_DIR_PIN     0x0002
+#define TMR2_PERIOD        3125
+
+// MOTOR 2 === motor on the left when youre facing the USB plugs
+
+typedef enum
+{
+	APP_STATE_INIT=0,
+	APP_STATE_SERVICE_TASKS,
+} APP_STATES;
+
+typedef struct
+{
+    APP_STATES state;
+} APP_DATA;
+
+
+/*
+ * fxn: initMotor1and2()
+ * -- set GPIO pins to put both motors forward
+ * -- enable OC1 and OC2 in PWM mode
+ * -- set original duty cycle to 0%
+ * -- turn on TIMER2 
+*/ 
+void initMotor1and2();
+
+// Set motor directions
+void setMotor1Forward();
+void setMotor2Forward();
+void setMotor1Backward();
+void setMotor2Backward();
+
+// Set Duty Cycles (percent)
+void setMotor1DC(unsigned char dc);
+void setMotor2DC(unsigned char dc);
+
+
+void APP_Initialize ( void );
+
+void APP_Tasks( void );
+
+
+#endif /* _APP_H */
