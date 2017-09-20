4f57ef286cfd38306a9a39c4e9468f54ead532af
Samuel McGhee <samhmcg5@vt.edu>
2017-09-19 20:38:21 -0400

    working better now

--- pwm_registers/firmware/src/system_config/default/system_interrupt.c
+++ pwm_registers/firmware/src/system_config/default/system_interrupt.c
@@ -3,12 +3,32 @@
 #include "system_definitions.h"
 
 
+int cm_count = 0;
+char state = 0;
     
     
 void __ISR(_TIMER_4_VECTOR, ipl1AUTO) IntHandlerDrvTmrInstance0(void)
 {
-    current_dc1 = current_dc1 - 10;
-    setMotor1DC(current_dc1);
+    //current_dc1 = current_dc1 - 10;
+    //setMotor1DC(current_dc1);
+    //setMotor1DC(0);
+    
+    if (cm_count % 10 == 0)
+    {
+        if (state == 0)
+        {
+            state = 1;
+            setMotor1Backward();
+            setMotor2Backward();
+        }
+        else
+        { 
+            state = 0;
+            setMotor1Forward();
+            setMotor2Forward();
+        }
+    }
+    cm_count++;
     
     PLIB_INT_SourceFlagClear(INT_ID_0,INT_SOURCE_TIMER_4);
 }
@@ -16,8 +36,9 @@ void __ISR(_TIMER_4_VECTOR, ipl1AUTO) IntHandlerDrvTmrInstance0(void)
 
 void __ISR(_TIMER_3_VECTOR, ipl1AUTO) IntHandlerDrvTmrInstance1(void)
 {
-    current_dc2 = current_dc2 - 10;
-    setMotor2DC(current_dc2);
+    //current_dc2 = current_dc2 - 10;
+    //setMotor2DC(current_dc2);
+    //setMotor2DC(0);
     
     PLIB_INT_SourceFlagClear(INT_ID_0,INT_SOURCE_TIMER_3);
 }
