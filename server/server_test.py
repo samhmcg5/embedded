043d002b16ce15922398598ab032e2cea23b592e
Eric Simpson <esimp12@vt.edu>
2017-09-21 08:45:16 -0400

    server uses log files now

--- server/server_test.py
+++ server/server_test.py
@@ -14,9 +14,15 @@ import json
 """2000"""
     
 dbcheck = None
-while dbcheck is None:
-    dbcheck = input("Is MongoDB Server on?")
+#while dbcheck is None:
+#    dbcheck = input("Is MongoDB Server on?")
+# connect to 
 mongo_client = MongoClient('localhost', 27017)
+
+# log file
+filename = "server_log.txt"
+file = open(filename, 'w')
+
 host = '192.168.1.123'
 port = 2000
 backlog = 5
@@ -25,13 +31,15 @@ size = 1
 buffer = ""
 data = ""
 ack_msg = '{ "direction": 1 }'
+
+# bind socket and beign to listen
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host,port))
 s.listen(backlog)
 
-print ("Waiting for a connection: ")    
+file.write("Waiting for a connection: \n")    
 client, address = s.accept()
-print ("Connected to WiFly: ")
+file.write("Connected to WiFly: \n")
 while True:
     data = client.recv(size).decode()
     if not data:
@@ -43,29 +51,28 @@ while True:
             break
         if "!" not in buffer:
             break
-        print("Received Message: ", buffer)
+        file.write("Received Message: " +  buffer + "\n")
         buffer = buffer[:-1]
         json_obj = json.loads(buffer)
-        pprint(json_obj)
+
         # store in db
         if 'store' in json_obj:
             db = mongo_client.test
-            print("Storing Item: ")
+            file.write("Storing Item: \n")
             result = db.testCollection.insert_one(json_obj)
             if result.acknowledged:
-                print("Sending acknowledgement: ")
+                file.write("Sending acknowledgement: \n")
                 client.send(ack_msg.encode())
         # retrieve from db
         elif 'retrieve' in json_obj:
-            print(json_obj['retrieve'])
             db = mongo_client.text
             json_str = '{ "store":' + str(json_obj['retrieve']) + ' }'
             json_obj_tmp = json.loads(json_str)
-            pprint(json_obj_tmp)
-            print("Retrieving Item: ")
+            file.write("Retrieving Item: \n")
             cursor = db.testCollection.find(json_obj_tmp)
-            print("Retrieved Item")
+            file.write("Sending acknowledgement: \n")
             client.send(ack_msg.encode())
         buffer = ""
         break
+file.close()
 client.close()
--- server/tcp_echo_client.py
+++ server/tcp_echo_client.py
@@ -9,11 +9,19 @@ import socket
 host = '192.168.1.123'
 port = 2000
 size = 1024
+filename = "client_log.txt"
+file = open(filename, 'w')
+count = 0;
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host,port))
-print("Connected to server: ")
+
+file.write("Connected to server: \n")
 while True:
-	json_str = input("Send a json message to server: ")
+	json_str = '{ "store":' + str(count) + ' }!' 
+	file.write("Sending to server: \n")
 	s.send(json_str.encode())
 	data = s.recv(size)
-	print ('Received:', data.decode())
+	file.write("Received: " + data.decode() + "\n")
+	count += 1
+file.close()
