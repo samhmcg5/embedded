638194eafc7eada0f0fe92744e8e4f970329bd74
Samuel McGhee <samhmcg5@vt.edu>
2017-11-08 11:49:32 -0500

    added data field class

--- server/IntegrationServers/database_fields.py
+++ server/IntegrationServers/database_fields.py
@@ -1,11 +1,11 @@
 # Database fields and globals
 host = '127.0.0.1'
 port = 27017
-dbname = 'team14'
-deliv_nav = 'deliv_nav'
+dbname      = 'team14'
+deliv_nav   = 'deliv_nav'
 deliv_sense = 'deliv_sense'
-scan_nav = 'scan_nav'
-scan_sense = 'scan_sense'
+scan_nav    = 'scan_nav'
+scan_sense  = 'scan_sense'
 
 # Database initialization messages
 INFO_DB_CONN_ATT        	= 'Attempting to connect to database.' 
@@ -28,55 +28,18 @@ ERROR_DB_STORE				= 'Database could not store data in... '
 ERROR_DB_RETR				= 'Database could not retrieve data from... '
 ERROR_DB_JSON				= 'Data being stored is not a JSON object!'
 
-# Default collection criteria for retrieves
-# ALL COLLECTIONS
-DEFAULT_CRITERIA = '{ "ACTION": { "$exists": true } }'
 
-# DELIV NAV
+class DelivNavFields:
+    col_name    = 'deliv_nav'
+    # master token for incoming JSON
+    token       = "DELIV_NAV"
+    # tokens to find which message type
+    tok_pos     = "X"
+    tok_status  = "STATUS"
+    tok_mag     = "SET_MAGNET"
+    # search criteria dicts
+    crit_pos    = "POS"
+    crit_status = "STATUS"
+    crit_mag    = 'SET_MAG'
 
-# DELIV SENSE
-
-# SCAN NAV
-
-# SCAN SENSE
-QUOTA_CRITERIA = '{ "QUOTA": { "$exists": true } }'
-
-# Default collection storage fields
-# DELIV NAV
-DELIV_NAV_DEFAULT_STATUS = '{ "SEQ": 0, "DELIV_NAV": { "STATUS": 0, "MSG": "Rover is IDLE, requesting task" } }'
-DELIV_NAV_TASK_ACK       = '{ "SEQ": 0, "DELIV_NAV": { "TASK": "RECEIVED" } }'
-DELIV_NAV_ACTION_ACK     = '{ "SEQ": 0, "DELIV_NAV": { "ACTION": "RECEIVED" } }'
-DELIV_NAV_DEFAULT_REQ    = '{ "SEQ": 0, "DELIV_NAV": { "SET_MAGNET": 0, "IR_DATA": 0 } }'
-DELIV_NAV_DEFAULT_POS    = '{ "SEQ": 0, "DELIV_NAV": { "X": 0, "Y": 20, "OR": 0 } }'
-DELIV_NAV_DEFAULT_ERR    = '{ "SEQ": 0, "DELIV_NAV": { "ERROR": 0, "MSG": "message" } }'
-DELIV_NAV_FIELDS = [DELIV_NAV_DEFAULT_STATUS,
-                    DELIV_NAV_TASK_ACK,
-                    DELIV_NAV_ACTION_ACK,
-                    DELIV_NAV_DEFAULT_REQ,
-                    DELIV_NAV_DEFAULT_POS,
-                    DELIV_NAV_DEFAULT_ERR]
-
-# DELIV SENSE
-DELIV_SENSE_DEFAULT_ACTION = '{ "SEQ": 0, "ACTION": 0 }'
-DELIV_SENSE_FIELDS = [ DELIV_SENSE_DEFAULT_ACTION ]
-
-# SCAN NAV
-SCAN_NAV_DEFAULT_ACTION = '{ "SEQ": 0, "ACTION": 0 }'
-SCAN_NAV_FIELDS = [ SCAN_NAV_DEFAULT_ACTION ]
-
-# SCAN SENSE
-SCAN_SENSE_DEFAULT_ZONE_ACTION 	= '{ "SEQ": 0, "ZONE": 1, "ACTION": 0 }' 
-SCAN_SENSE_DEFAULT_ZONE_1_CURR 	= '{ "SEQ": 0, "SCAN_SENSE":{ "ZONE": 1, "RED": 0, "GREEN": 0, "BLUE": 0 } }'
-SCAN_SENSE_DEFAULT_ZONE_2_CURR 	= '{ "SEQ": 0, "SCAN_SENSE":{ "ZONE": 2, "RED": 0, "GREEN": 0, "BLUE": 0 } }'
-SCAN_SENSE_DEFAULT_ZONE_3_CURR 	= '{ "SEQ": 0, "SCAN_SENSE":{ "ZONE": 3, "RED": 0, "GREEN": 0, "BLUE": 0 } }'
-SCAN_SENSE_DEFAULT_ZONE_1_QUOTA = '{ "SEQ": 0, "QUOTA":{ "ZONE": 1, "RED": 0, "GREEN": 0, "BLUE": 0 } }'
-SCAN_SENSE_DEFAULT_ZONE_2_QUOTA = '{ "SEQ": 0, "QUOTA":{ "ZONE": 2, "RED": 0, "GREEN": 0, "BLUE": 0 } }'
-SCAN_SENSE_DEFAULT_ZONE_3_QUOTA = '{ "SEQ": 0, "QUOTA":{ "ZONE": 3, "RED": 0, "GREEN": 0, "BLUE": 0 } }'
-SCAN_SENSE_FIELDS = [ SCAN_SENSE_DEFAULT_ZONE_ACTION, 
-					SCAN_SENSE_DEFAULT_ZONE_1_CURR, 
-					SCAN_SENSE_DEFAULT_ZONE_2_CURR, 
-					SCAN_SENSE_DEFAULT_ZONE_3_CURR,
-					SCAN_SENSE_DEFAULT_ZONE_1_QUOTA,
-					SCAN_SENSE_DEFAULT_ZONE_2_QUOTA,
-					SCAN_SENSE_DEFAULT_ZONE_3_QUOTA ]
 
--- server/IntegrationServers/deliv_nav_thread.py
+++ server/IntegrationServers/deliv_nav_thread.py
@@ -1,9 +1,17 @@
 from base_thread import ServerBaseThread
-from server_fields import *
+from database_fields import DelivNavFields as DNF
 
+"""
+MONGO USAGE:
 
-# Try this:
-# collection.update({'_id':book["_id"]},book)  
+Each field is stored under a specific search criteria:
+--> position  = {"X":0, "Y":0, "OR":0} is stored under "POS"
+        which gives us a document {"POS" : {"X":0, "Y":0, "OR":0} }
+        use a search dictionary {"POS":{"$exists":True}} to retrieve the last one
+--> status = {"STATUS":1}, (0=IDLE,1=EXEC_TASK,2=EXEC_ACTION)
+--> set magnet = {"SET_MAG":1}, (1=ON, 0=OFF)
+
+"""
 
 class DelivNavThread(ServerBaseThread):
     def __init__(self, ip, port, status_thread):
@@ -11,32 +19,39 @@ class DelivNavThread(ServerBaseThread):
         self.name = "DelivNav"
 
     def handleJSON(self, json_obj):
-        if not DELIV_NAV in json_obj:
+        if not DNF.token in json_obj:
            return
         
         if self.recv_seq+1 != json_obj["SEQ"]:
-            self.sendToStatus("Unexpected sequence number")
+            self.sendToStatus("ERROR: Unexpected sequence number")
         self.recv_seq = json_obj["SEQ"]
+        
         # get the data out of it
-        delivnav = json_obj[DELIV_NAV]
+        delivnav = json_obj[DNF.token]
 
         # now take an action based on the data ...
         # POSTITION
-        # if "X" in delivnav.keys():
-            # self.srv.store({"X":{"$exists":True}}, delivnav, self.srv.db.deliv_nav)
-            # data = self.srv.retrieve({"X":{"$exists":True}}, self.srv.db.deliv_nav)
+        if DNF.tok_pos in delivnav.keys():
+            # simply store the position
+            json_store = {DNF.crit_pos : delivnav}
+            self.srv.store({DNF.crit_pos : {"$exists":True}}, json_store, DNF.col_name)
+            # data = self.srv.retrieve({"POS":{"$exists":True}}, self.srv.db.deliv_nav)
             # self.sendToStatus(str(data))
+        
         # IDLE, NEED A TASK
-        if "STATUS" in delivnav.keys():
-            dictstr = {"STATUS":{"$exists":"true"}}
-            json_obj = {"STATUS":json_obj}
-            self.srv.store(dictstr, json_obj, self.srv.db.deliv_nav)
-            data = self.srv.retrieve(dictstr, self.srv.db.deliv_nav)
-            self.sendToStatus(str(data))
-            # if "IDLE" in delivnav["MSG"]:
-                # Get a task
-                # ensure the task is eventually acked, maybe use a time out period?
-        # SET MAGNET ON OR OFF
-        # elif "SET_MAGNET" in delivnav.keys():            
-            # self.srv.store({"SET_MAGNET":{"$exists":True}}, delivnav, self.srv.db.deliv_nav)
+        if DNF.tok_status in delivnav.keys():
+            # store the current status
+            json_store = {DNF.crit_status : delivnav["STATUS"]}
+            self.srv.store({DNF.crit_status : {"$exists":True}}, json_store, DNF.col_name)
+            if delivnav["STATUS"] == 0:
+                # request new task ...
+                # send new task to rover ...
+                # start timer for task acknowledge status ...
+                pass
 
+        # SET MAGNET ON OR OFF
+        elif DNF.tok_mag in delivnav.keys():            
+            json_store = {DNF.crit_mag : delivnav["SET_MAGNET"]}
+            self.srv.store({DNF.crit_mag : {"$exists":True}}, json_store, DNF.col_name)
+            # now request the state of the magnet ...
+            # send the state of the magnet back to the rover ...
