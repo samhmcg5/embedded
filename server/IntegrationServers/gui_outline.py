f5eb8ef7b5b906f1d247d231e523425088271b0a
Samuel McGhee <samhmcg5@vt.edu>
2017-11-09 12:57:56 -0500

    made it pretty

--- server/IntegrationServers/gui_outline.py
+++ server/IntegrationServers/gui_outline.py
@@ -2,7 +2,10 @@ from PyQt5.QtWidgets import QWidget, QApplication
 import PyQt5.QtWidgets as qtw
 import PyQt5.QtCore as qtc
 from PyQt5.QtGui import QFont
+
 import sys
+
+from styles import stylesheet
 
 ###############################
 ### TOP LEVEL GUI COMPONENT ###
@@ -11,7 +14,7 @@ class TopLevelUI(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
-
+        self.setStyle()
     def initUI(self): 
         f = QFont()
         f.setPointSize(18)
@@ -19,16 +22,16 @@ class TopLevelUI(QWidget):
         self.quotaframe  = SetQuotaFrame()
         self.currentnums = CurrentNumbersFrame()
         self.delivstats  = DelivNavStatusFrame()
-
         vbox = qtw.QVBoxLayout()
         vbox.addWidget(self.quotaframe)
         vbox.addWidget(self.currentnums)
         vbox.addWidget(self.delivstats)
         self.setLayout(vbox)
-
         self.setGeometry(300, 300, 800, 500)
         self.setWindowTitle('Server Control')
         self.show()
+    def setStyle(self):
+        self.setStyleSheet(stylesheet)
 
 ######################
 ### SET THE QUOTAS ###
@@ -37,7 +40,6 @@ class SetQuotaFrame(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
-
     def initUI(self):
         vbox = qtw.QVBoxLayout()
         self.zoneA = QuotaRow("Zone A")
@@ -57,13 +59,13 @@ class QuotaRow(QWidget):
         self.name = name
         self.initUI()
     def initUI(self):
+        self.red = SpinBoxQuota("Red",5)
+        self.green = SpinBoxQuota("Green",5)
+        self.blue = SpinBoxQuota("Blue",5)
         hbox = qtw.QHBoxLayout()
         hbox.addWidget(qtw.QLabel(self.name + ":\t"))
-        self.red = SpinBoxQuota("Red",5)
         hbox.addWidget(self.red)
-        self.green = SpinBoxQuota("Green",5)
         hbox.addWidget(self.green)
-        self.blue = SpinBoxQuota("Blue",5)
         hbox.addWidget(self.blue)
         self.setLayout(hbox)
 
@@ -80,12 +82,11 @@ class CurrentNumbersFrame(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
-
     def initUI(self):
-        vbox = qtw.QVBoxLayout()
         self.zoneA = NumsRow("Zone A")
         self.zoneB = NumsRow("Zone B")
         self.zoneC = NumsRow("Zone C")
+        vbox = qtw.QVBoxLayout()
         vbox.addWidget(qtw.QLabel("CURRENT NUMBERS"))
         vbox.addWidget(self.zoneA)
         vbox.addWidget(self.zoneB)
@@ -98,13 +99,13 @@ class NumsRow(QWidget):
         self.name = name
         self.initUI()
     def initUI(self):
+        self.red = qtw.QLabel("Red = 0")
+        self.green = qtw.QLabel("Green = 0")
+        self.blue = qtw.QLabel("Blue = 0")
         hbox = qtw.QHBoxLayout()
         hbox.addWidget(qtw.QLabel(self.name + ":\t"))
-        self.red = qtw.QLabel("Red = 0")
         hbox.addWidget(self.red)
-        self.green = qtw.QLabel("Green = 0")
         hbox.addWidget(self.green)
-        self.blue = qtw.QLabel("Blue = 0")
         hbox.addWidget(self.blue)
         self.setLayout(hbox)
     def setNums(self, r, g, b):
@@ -119,27 +120,22 @@ class DelivNavStatusFrame(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
-
     def initUI(self):
         vbox = qtw.QVBoxLayout()
         self.execStatus = ExecStatus()
-        # self.posView = CurrentPos()
-        # self.setPos   = SetPos()
         self.posGrid = PositionGrid()
         vbox.addWidget(qtw.QLabel("DELIVERY STATUS"))
         vbox.addWidget(self.execStatus)
-        # vbox.addWidget(self.posView)
-        # vbox.addWidget(self.setPos)
         vbox.addWidget(self.posGrid)
         self.setLayout(vbox)
 
 class ExecStatus(QWidget):
     def __init__(self):
         super().__init__()
-        hbox = qtw.QHBoxLayout()
-        hbox.addWidget(qtw.QLabel("Currently:\t"))
         self.status = qtw.QLabel("IDLE")
-        hbox.addWidget(self.status)
+        hbox = qtw.QHBoxLayout()
+        hbox.addWidget(qtw.QLabel("Currently: "))
+        hbox.addWidget(self.status, alignment=qtc.Qt.AlignLeft, stretch=1)
         self.setLayout(hbox)
     def setStatus(msg):
         self.status.setText(msg)
@@ -149,9 +145,9 @@ class PositionGrid(QWidget):
         super().__init__()
         self.initUI()
     def initUI(self):
-        self.x   = qtw.QLabel("X: 0")
-        self.y   = qtw.QLabel("Y: 20")
-        self.ori = qtw.QLabel("OR: 0")
+        self.x    = qtw.QLabel("X: 0")
+        self.y    = qtw.QLabel("Y: 20")
+        self.ori  = qtw.QLabel("OR: 0")
         self.nx   = SpinBoxQuota("X",90)
         self.ny   = SpinBoxQuota("Y",50)
         self.nori = SpinBoxQuota("OR",359)
@@ -168,42 +164,6 @@ class PositionGrid(QWidget):
         gbox.addWidget(self.sendButton,1,4)
         self.setLayout(gbox)
 
-
-class CurrentPos(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-    def initUI(self):
-        self.x = qtw.QLabel("X: 0")
-        self.y = qtw.QLabel("Y: 20")
-        self.ori = qtw.QLabel("OR: 0")
-        hbox = qtw.QHBoxLayout()
-        hbox.addWidget(qtw.QLabel("DelivNav Position:\t"))
-        hbox.addWidget(self.x)
-        hbox.addWidget(self.y)
-        hbox.addWidget(self.ori)
-        self.setLayout(hbox)
-    def setNums(self, x, y, ori):
-        self.x.setText("X : %i" % x)
-        self.y.setText("Y : %i" % y)
-        self.ori.setText("OR : %i" % ori)
-
-class SetPos(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-    def initUI(self):
-        self.x = SpinBoxQuota("X",90)
-        self.y = SpinBoxQuota("Y",50)
-        self.ori = SpinBoxQuota("OR",359)
-        self.sendButton = qtw.QPushButton("Send Position")
-        hbox = qtw.QHBoxLayout()
-        hbox.addWidget(qtw.QLabel("Corrected Position"))
-        hbox.addWidget(self.x)
-        hbox.addWidget(self.y)
-        hbox.addWidget(self.ori)
-        hbox.addWidget(self.sendButton)
-        self.setLayout(hbox)
 
 ########################
 ### EXECUTE THE MAIN ###
