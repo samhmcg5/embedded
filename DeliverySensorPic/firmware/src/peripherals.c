3b9714857e4ff30cd582ed3c03133a5932704e15
Rkihtrak <ramkar94@gmail.com>
2017-11-09 17:53:00 -0500

    cleaned up

--- DeliverySensorPic/firmware/src/peripherals.c
+++ DeliverySensorPic/firmware/src/peripherals.c
@@ -16,12 +16,6 @@ void sendMsgToPeriphQFromISR(struct periphQueueData msg){
 }
 void setMagnet(bool on){
     if(on == MAGNET_STATE_ON){
-        
-//        TRISBCLR = 0x0400;
-//        ODCBCLR  = 0x0400;
-//        
-//        LATBCLR = 0x0400;
-//        LATBSET = 0x0400;//RB11 is my pin
         TRISFCLR = 0x0008;
         ODCFCLR  = 0x0008;
         
@@ -40,14 +34,10 @@ void setMagnet(bool on){
 
 void setADC(bool on){
     if(on == ADC_STATE_ON){
-    //enable ADC
         DRV_ADC_Open();
-//        DRV_ADC_Start();
         adc_state = true;
     }
     else{
-    //disable ADC
-//        DRV_ADC_Stop();
         DRV_ADC_Close();
         adc_state = false;
     }
@@ -85,11 +75,8 @@ void PERIPHERALS_Tasks ( void )
 
         case PERIPHERALS_STATE_SERVICE_TASKS:
         {
-            //do my peripherals shit
-            //setADC(ADC_STATE_ON);
             DRV_ADC_Open();
             DRV_TMR0_Start();
-            //setMagnet(MAGNET_STATE_ON);
             struct periphQueueData recv;
             while(1){
                 if(xQueueReceive(periph_q, &recv, portMAX_DELAY)){
@@ -100,32 +87,14 @@ void PERIPHERALS_Tasks ( void )
                         Nop();
                         if(recv.magnet == true){
                             setMagnet(MAGNET_STATE_ON);
-                            
-                            
-                            
-                            //uart send magnet on
-                             //char buf[55];
-                            //sprintf(buf, STR_MAGNET_STATE, outgoing_seq, magnet_state);
-                            //commSendMsgToUartQueue(buf);
-                            /**
-                            #define STR_MAGNET_STATE	"{\"SEQ\":%i,\"DELIV_SENSE\": { \"MAGNET\":%i}}!"
-                            #define STR_IR_DISTANCE 	"{\"SEQ\":%i,\"DELIV_SENSE\":{\"IRDIST\":%i}}!"
-                             **/
                         }
                         else{
                             setMagnet(MAGNET_STATE_OFF);
-                            
-                            
-                            //char buf[55];
-                            //sprintf(buf, STR_MAGNET_STATE, outgoing_seq, magnet_state);
-                            //commSendMsgToUartQueue(buf);
-                            //uart send magnet off
                         }
                     }
                     else if(recv.type == ADC_TO_PERIPHERAL_MSG){
                         dbgOutputLoc(PERIPH_ADC);
                         Nop();
-                        //double convert = 4118.6/(recv.distance) + 0.5;
                         double convert = 2076.0/(recv.distance - 11.0);
                        objDist = convert;
                        if(objDist < 5.0)
@@ -137,8 +106,6 @@ void PERIPHERALS_Tasks ( void )
                         dbgOutputLoc(PERIPH_TMR);
                         Nop();
                         if(objDist != 0){
-                        //send uart
-                        //#define STR_IR_DISTANCE 	"{\"SEQ\":%i,\"DELIV_SENSE\":{\"IRDIST\":%i}}!"
                             char buf[55];
                             sprintf(buf, STR_IR_DISTANCE, outgoing_seq, objDist, objHeld);
                             commSendMsgToUartQueue(buf);
@@ -151,14 +118,8 @@ void PERIPHERALS_Tasks ( void )
                 }
             }
         }
-
-        /* TODO: implement your application state machine.*/
-        
-
-        /* The default state should never be executed. */
         default:
         {
-            /* TODO: Handle error in application's state machine. */
             break;
         }
     }
